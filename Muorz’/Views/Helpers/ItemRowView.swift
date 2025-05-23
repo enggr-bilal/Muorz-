aea5289b754fdbc83eeb8d3a08ecad5fcc7f1f3e
Simon NAUD <162438554+nomisduan@users.noreply.github.com>
2025-05-23 16:50:02 +0200

    row created

---
+++ Muorz’/Views/Helpers/ItemRowView.swift
@@ -0,0 +1,18 @@
+//
+//  IteRowView.swift
+//  Muorz’
+//
+//  Created by Simon Naud on 23/05/25.
+//
+
+import SwiftUI
+
+struct IteRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IteRowView()
+}
---
+++ Muorz’/Views/Helpers/QuantityPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  PickerView.swift
+//  Muorz’
+//
+//  Created by Simon Naud on 23/05/25.
+//
+
+import SwiftUI
+
+struct PickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PickerView()
+}
